5f66ef86e3657a9067f954806cc9c3355ecd63a4
Mateusz <dsw55990@student.dsw.edu.pl>
2025-05-06 21:05:22 +0200

    Dodano funkcję tworzenia plików log

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-if [ "$1" == "--date" ]; then
-  date
+if [ "$1" == "--logs" ]; then
+  count=${2:-100}
+  for ((x=1; x<=$count; x++)); do
+    echo "Nazwa pliku: log$x.txt" > log$x.txt
+    echo "Nazwa skryptu: skrypt.sh" >> log$x.txt
+    echo "Data utworzenia: $(date)" >> log$x.txt
+  done
 fi
